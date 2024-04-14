731288d02088e0b7faf9d450b437a59ba026fdaa
Nataliia-Homyk324 <homyk324@gmail.com>
2024-04-14 21:07:10 +0300

    SearchBar

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+// import css from './App.module.css';
+// import { getImagesUnplash } from '../../images-api';
+import SearchBar from '../SearchBar/SearchBar';
+
+export default function App() {
+  return (
+    <>
+      <SearchBar />
+    </>
+  );
+}
