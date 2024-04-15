178b725e704cb1317d6f68be184818ad9ee39aa3
Nataliia-Homyk324 <homyk324@gmail.com>
2024-04-15 23:02:01 +0300

    Loader

--- src/images-api.js
+++ src/images-api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 axios.defaults.baseURL = `https://api.unsplash.com/`;
-const ACCESS_KEY = `YaJJTeBQ6DHMEJ5Vo5r9CiLdVdupIp0mtFcd7X5uyDo`;
+const ACCESS_KEY = `5aQ2Sn_zXCYuguK_Au9bJPuNle7L3L5mD28kmOhkO0Y`;
 
 export const getImagesUnplash = async (searchImg, pageNumber) => {
     const params = {
