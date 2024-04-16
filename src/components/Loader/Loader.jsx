4f26ee415f9805470c0304b579649c1a9630ab03
Nataliia-Homyk324 <homyk324@gmail.com>
2024-04-16 08:51:06 +0300

    loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,18 +1,18 @@
-import { Hourglass } from 'react-loader-spinner';
+import { Grid } from 'react-loader-spinner';
 import css from './Loader.module.css';
 
 const Loader = () => {
   return (
     <div className={css.container}>
-      <Hourglass
-        className={css.loader}
+      <Grid
         visible={true}
         height="80"
         width="80"
-        ariaLabel="hourglass-loading"
+        color="#5c6297"
+        ariaLabel="grid-loading"
+        radius="12.5"
         wrapperStyle={{}}
-        wrapperClass=""
-        colors={['#5c6297', '#fff']}
+        wrapperClass="grid-wrapper"
       />
     </div>
   );
