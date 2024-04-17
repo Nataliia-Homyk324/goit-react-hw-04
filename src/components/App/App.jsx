b0b8509ef6d2aadf5182f24e1fe88360a6c6ebd0
Nataliia-Homyk324 <homyk324@gmail.com>
2024-04-17 17:19:44 +0300

    Modal

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import Modal from 'react-modal';
 import ImageGallery from '../ImageGallery/ImageGallery';
 import SearchBar from '../SearchBar/SearchBar';
 import { fetchImages } from '../../images-api';
@@ -8,18 +9,27 @@ import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import toast, { Toaster } from 'react-hot-toast';
 import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
 
+import ImageModal from '../ImageModal/ImageModal';
+
 export default function App() {
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
-
   const [page, setPage] = useState(1);
   const [query, setQuery] = useState('');
+
+  const [selectedImage, setSelectedImage] = useState(null); // Стан для зберігання вибраного зображення для модального вікна
+  const [modalIsOpen, setModalIsOpen] = useState(false); // Стан для відображення/приховування модального вікна
+
+  useEffect(() => {
+    Modal.setAppElement('#root');
+  }, []);
 
   const handleSearch = newQuery => {
     setQuery(newQuery);
     setPage(1);
-    setImages([]);
+    setImages([]); // Reset the Gallery when performing a new search
+    setError(false); // Reset error state when performing a new search
   };
 
   const handleLoadMore = () => {
@@ -50,7 +60,7 @@ export default function App() {
               duration: 5000,
             }
           );
-        } else {
+        } else if (page < 2) {
           toast.success(`Wow! We found ${data.total} pictures`);
         }
       } catch (error) {
@@ -63,20 +73,32 @@ export default function App() {
     getImages();
   }, [page, query]);
 
-  console.log(images);
+  const openModal = image => {
+    setSelectedImage(image);
+    setModalIsOpen(true);
+  };
+  const closeModal = () => setModalIsOpen(false);
 
   return (
     <div className={css.container}>
       <SearchBar onSearch={handleSearch} />
       <Toaster />
+      {error && <ErrorMessage />}
 
-      {images.length > 0 && <ImageGallery items={images} />}
+      {images.length > 0 && (
+        <ImageGallery items={images} openModal={openModal} />
+      )}
 
       {isLoading && <Loader />}
-      {error && <ErrorMessage />}
-      {images.length > 0 && !isLoading && (
+
+      {images.length > 9 && !isLoading && (
         <LoadMoreBtn onClick={handleLoadMore} />
       )}
+      <ImageModal
+        isOpen={modalIsOpen}
+        image={selectedImage}
+        onCloseModal={closeModal}
+      />
     </div>
   );
 }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,12 +1,23 @@
 import ImageCard from '../ImageCard/ImageCard';
 import css from './ImageGallery.module.css';
 
-export default function ImageGallery({ items }) {
+export default function ImageGallery({ items, openModal }) {
+  const imageClick = event => {
+    const imgItem = event.target.closest('li');
+    if (imgItem) {
+      const imgID = imgItem.dataset.id;
+      const clickedImageItem = items.find(image => image.id === imgID);
+      if (clickedImageItem) {
+        openModal(clickedImageItem);
+      }
+    }
+  };
+
   return (
     <section className={css.containerGallery}>
-      <ul className={css.gallery}>
+      <ul className={css.gallery} onClick={imageClick}>
         {items.map(item => (
-          <li className={css.galleryItem} key={item.id}>
+          <li className={css.galleryItem} key={item.id} data-id={item.id}>
             <ImageCard imageItem={item} />
           </li>
         ))}
